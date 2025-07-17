f7ee3b1770abda14f1fe621f0010657ebedd6c6c
Сергей <amoc@list.ru>
2025-07-17 11:06:50 +0300

    база  и вьюха ок

--- app/views.py
+++ app/views.py
@@ -1,45 +1,40 @@
-"""
-Definition of views.
-"""
+# ver1.0.0 не загружено
 
-from datetime import datetime
-from django.shortcuts import render
-from django.http import HttpRequest
+import json
+from django.http import HttpResponse, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from .async_bot import bot
+from telebot.util import Update
+import logging
+# ver 1.0.0 не загружено
+# логирование django
+logger = logging.getLogger(__name__)  #
 
-def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        }
-    )
+logger.info(f'the module {__name__} running')
 
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
+@csrf_exempt
+async def telegram_webhook(request):
+    try:
+        if request.method == 'POST':
+            # Проверка секретного токена (опционально)  - разобраться
+            if request.headers.get('X-Telegram-Bot-Api-Secret-Token') != settings.TELEGRAM_WEBHOOK_SECRET:
+                # return HttpResponseForbidden("Invalid token")
+                logger.info(f'the secret key in telegram request {__name__} incorrect')
 
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+
+            # Асинхронная обработка
+            update_json = json.loads(await request.aread())
+            update = Update.de_json(update_json)
+            await bot.process_new_updates([update])
+            return HttpResponse("OK")
+            logger.info(f'{__name__} OK')
+
+        return HttpResponse("Only POST allowed")
+        logger.info(f'{__name__} incorrect request.method. Only POST allowed')
+
+    except Exception as e:
+
+        logger.error(f"Error checking subscription: {e}")
+        # print(f"Error checking subscription: {e}")
+        return False
